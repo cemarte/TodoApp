b1969cb9cdb648aa28917f9f07a91b2c6b4a0ed5
Celso Marciano <cemarte83@gmail.com>
2022-12-04 13:10:11 -0500

    Before rewrite in Go

--- src/client/todo-react/src/components/Todo.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export const Todo = ({ onClick, isDone, text }: { onClick: () => void, isDone: boolean, text: string }) => (
-    <li onClick={onClick} style={{ textDecoration: isDone ? 'line-through' : 'none' }}>{text}</li>
-);
